e0cd288af692c937c34860317e07adf2f834aad6
Taylor Daughtry <taylordaughtry@users.noreply.github.com>
2019-03-14 15:08:40 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <title>Demo Project</title>
 </head>
 <body>
-    <h1>This Title is being updated in a Pull Request</h1>
+    <h1>This Title is being updated in the Pull Request</h1>
     @foreach ($entries as $entry)
         <h2>{{ $entry->title }}</h2>
         <p>{{ $entry->body }}</p>
